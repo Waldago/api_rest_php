d0dbf47a90b5e4854a47f42ea62cc68300180288
waldago <waldago@hotmail.com>
2023-03-02 09:32:30 -0300

    Creacion de proyecto apirest

---
+++ apirest_dinamica/Controllers/routes.controller.php
@@ -0,0 +1,13 @@
+<?php 
+
+    class RoutesController{
+
+        // ruta principal
+
+        public function index(){
+
+            include_once("Routes/routes.php");
+
+        }
+    }
+?>
---
+++ apirest_dinamica/Routes/routes.php
@@ -0,0 +1,72 @@
+<?php 
+    //Armo un array con todo lo que este cerca de la "/"    
+    $routesArray=explode("/",$_SERVER['REQUEST_URI']);
+    //Limpio el array
+    $routesArray=array_filter($routesArray);
+    
+    //Cuando no se hace una peticion a la api
+
+    if(empty($routesArray)){
+        $json = array (
+            'status'=>404,
+            'result'=>'Not Found'
+        );
+    
+        echo json_encode($json, http_response_code($json["status"]));
+    
+        return;
+    }
+
+    //Cuando se hace una peticion a la api
+    
+    if(!empty($routesArray) && isset($_SERVER['REQUEST_METHOD'] )){
+
+        echo '<pre>'; print_r($_SERVER['REQUEST_METHOD']); echo '</pre>';
+
+        if($_SERVER['REQUEST_METHOD']=='GET'){
+            $json = array (
+                'status'=>200,
+                'result'=>'Solicitud GET'
+            );
+        
+            echo json_encode($json, http_response_code($json["status"]));
+        
+            return;
+        }
+
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+            $json = array (
+                'status'=>200,
+                'result'=>'Solicitud POST'
+            );
+        
+            echo json_encode($json, http_response_code($json["status"]));
+        
+            return;
+        }
+
+        if($_SERVER['REQUEST_METHOD']=='PUT'){
+            $json = array (
+                'status'=>200,
+                'result'=>'Solicitud PUT'
+            );
+        
+            echo json_encode($json, http_response_code($json["status"]));
+        
+            return;
+        }
+
+        if($_SERVER['REQUEST_METHOD']=='DELETE'){
+            $json = array (
+                'status'=>200,
+                'result'=>'Solicitud DELETE'
+            );
+        
+            echo json_encode($json, http_response_code($json["status"]));
+        
+            return;
+        }
+    }
+    
+    
+?>
